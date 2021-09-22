a751891b2354fb041ae93ab5a2d637af8f12a95c
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-09-21 20:56:21 -0400

    working on navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,9 +11,19 @@ function Nav(props) {
     document.title = (currentSection.name);
   }, [currentSection]);
 
+
   return (
     <div>
-      <nav>
+      <nav className="navigation">
+        <ul className="navbar">
+          {sections.map((section) => (
+            <li className={`mainEl ${
+              currentSection.name === section.name && 'navActive font-weight-bold'
+              }`} key={section.name}>
+              <span onClick={() => {setCurrentSection(section)}} > {(section.name)}</span>
+            </li>
+          ))}
+          </ul>
         {/* navigation that should include: HOME, ABOUT, PREVIOUS YEARS, ORGANIZERS */}
       </nav>
     </div>
