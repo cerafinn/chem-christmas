8f6e153c3b41c29d1e525ef31754c2c5bc504176
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-09-29 23:53:26 -0400

    working on body switch case

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-function Body() {
+import Home from '../../Pages/Home';
+import About from '../../Pages/About';
+import Gallery from '../Gallery';
+import Profiles from '../Profiles';
+import Contact from '../../Pages/Contact';
+
+function Body({ currentSection }) {
+  const currentBody = () => {
+    switch (currentSection.name) {
+      case 'home':
+        return <Home />
+      case 'about':
+        return <About />
+      case 'gallery':
+        return <Gallery />
+      case 'profiles':
+        return <Profiles />
+      case 'contact':
+        return <Contact />
+      default:
+        return <Home />
+    }
+  }
+
   return (
     <div>
       {/* how react generates the main section of the page based on what is clicked in the nav, switch case */}
+      {currentBody()}
     </div>
   )
 };
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Gallery () {
+function Gallery() {
 
   return (
     <div>
